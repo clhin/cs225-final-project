0f40d4e3dfdd0e8d0746adceec7bcadd556d6698
johnhof3 <95899793+johnhof3@users.noreply.github.com>
2022-11-26 17:55:56 -0600

    correct graph files

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,503 +1,304 @@
-#include "graph.hpp"
+#include "graph.h"
+#include <map>
+#include <vector>
+#include <string>
+#include <fstream>
+#define NATION_COUNT 200 // placeholder if we use vectors
 
+// List of Variables :
+// t: year
+// i: exporter
+// j: importer
+// k: product
+// v: value 
+// q: quantity
+
+
+//Takes csv file input of the trade data and constructs the vector of vectors graph; an adjacency matrix
 Graph::Graph(const std::string& db_fpath) {
-	std::string line;
-	// create an empty matrix to start with
-	for (uint x = 0; x < 190; x++) {
-		std::vector<int> tmpvect;
-		for (uint y = 0; y < 190; y++) {
+	for (uint x = 0; x <= 237; x++) {
+		std::vector<float> tmpvect;
+		for (uint y = 0; y <= 237; y++) {
 			tmpvect.push_back(0);
 		}
 		graph.push_back(tmpvect);
-	} 
+	}
+	int lineCount = 0;
+    std::string line;
 	std::ifstream db(db_fpath);
-	while (std::getline(db, line, '\n')) {
-		int count = 0, x = -1, y = -1;
+	while(getline(db, line, '\n')) {
 		size_t pos;
 		std::string token;
+		int exporter, importer;
+		float productVal;
+		unsigned count = 0;
 		while((pos = line.find(',')) != std::string::npos) {
-			// we only care about two lines, country and group
 			token = line.substr(0, pos);
-			if (count != 7 && count != 58) {
+			if (count != 1 && count != 2 && count != 4) {
 				line.erase(0, pos + 1);
 				count++;
 				continue;
 			}
-			if (count == 7) {
-				x = countrycodes(stoi(token));
+			if(lineCount != 0){
+				if(count == 1)
+					exporter = countrycodes(std::stoi(token));
+				if(count == 2)
+					importer = countrycodes(std::stoi(token));
+            	if(count == 4)
+            		productVal = std::stof(token);
 			}
-			if (count == 58) {
-				y = terrorcodes(token);
-			}
+
 			line.erase(0, pos + 1);
 			count++;	
 		}
-		if (x >= 0 && y >= 0)
-			graph[x][y]++;
+		if(lineCount != 0)
+			graph[exporter][importer] += productVal;
+		lineCount++;
 	}
 }
 
-int Graph::countrycodes(int i) {
-	switch (i) {
-		case 4:
-			return AFGHANISTAN;
-		case 5:
-			return ALBANIA;
-		case 6:
-			return ALGERIA;
-		case 7:
-			return ANDORRA;
-		case 8:
-			return ANGOLA;
-		case 10:
-			return ANTIGUABARBUDA;
-		case 11:
-			return ARGENTINA;
-		case 12:
-			return ARMENIA;	
-		case 14:
-			return AUSTRALIA;
-		case 15:
-			return AUSTRIA;
-		case 16:
-			return AZERBAIJAN;
-		case 17:
-			return BAHAMAS;
-		case 18:
-			return BAHRAIN;
-		case 19:
-			return BANGLADESH;
-		case 20:
-			return BARBADOS;
-		case 21:
-			return BELGIUM;
-		case 22:
-			return BELIZE;
-		case 23:
-			return BENIN;
-		case 24:
-			return UNITEDKINGDOM;
-		case 25:
-			return BHUTAN;
-		case 26:
-			return BOLIVIA;
-		case 28:
-			return BOSNIAHERZEGOVNIA;
-		case 29:
-			return BOTSWANA;
-		case 30:
-			return BRAZIL;
-		case 31:
-			return BRUNEI;
-		case 32:
-			return BULGARIA;
-		case 33:
-			return BURKINAFASO;
-		case 34:
-			return BURUNDI;
-		case 35:
-			return BELARUS;
-		case 36:
-			return CAMBODIA;
-		case 37:
-			return CAMEROON;
-		case 38:
-			return CANADA;
-		case 41:
-			return CENTRALAFRICANREPUBLIC;
-		case 42:
-			return CHAD;
-		case 43:
-			return CHILE;
-		case 44:
-			return CHINA;
-		case 45:
-			return COLOMBIA;
-		case 46:
-			return COMOROS;
-		case 47:
-			return REPUBLICOFCONGO;
-		case 49:
-			return COSTARICA;
-		case 50:
-			return CROATIA;
-		case 51:
-			return CUBA;
-		case 53:
-			return CYPRUS;
-		case 54:
-			return CZECHREPUBLIC;
-		case 55:
-			return DENMARK;
-		case 56:
-			return DJIBOUTI;
-		case 57:
-			return DOMINICA;
-		case 58:
-			return DOMINICANREPUBLIC;
-		case 59:
-			return ECUADOR;
-		case 60:
-			return EGYPT;
-		case 61:
-			return ELSALVADOR;
-		case 62:
-			return EQUATORIALGUINEA;
-		case 63:
-			return ERITREA;
-		case 64:
-			return ESTONIA;
-		case 65:
-			return ETHIOPIA;
-		case 66:
-			return FALKLANDISLANDS;
-		case 67:
-			return FIJI;
-		case 68:
-			return FINLAND;
-		case 69:
-		case 70:
-		case 71:
-			return FRANCE;
-		case 72:
-			return GABON;
-		case 73:
-			return GAMBIA;
-		case 74:
-			return GEORGIA;
-		case 75:
-			return GERMANY;
-		case 76:
-			return GHANA;
-		case 78:
-			return GREECE;
-		case 79:
-			return DENMARK;
-		case 80:
-			return GRENADA;
-		case 81:
-			return GUADELOUPE;
-		case 83:
-			return GUATEMALA;
-		case 84:
-			return GUINEA;
-		case 85:
-			return GUINEABISSAU;
-		case 86:
-			return GUYANA;
-		case 87:
-			return HAITI;
-		case 88:
-			return HONDURAS;
-		case 89:
-			return CHINA;
-		case 90:
-			return HUNGARY;
-		case 91:
-			return ICELAND;
-		case 92:
-			return INDIA;
-		case 93:
-			return INDONESIA;
-		case 94:
-			return IRAN;
-		case 95:
-			return IRAQ;
-		case 96:
-			return IRELAND;
-		case 97:
-			return ISRAEL;
-		case 98:
-			return ITALY;
-		case 99:
-			return IVORYCOAST;
-		case 100:
-			return JAMAICA;
-		case 101:
-			return JAPAN;
-		case 102:
-			return JORDAN;
-		case 103:
-			return KAZAKHSTAN;
-		case 104:
-			return KENYA;
-		case 106:
-			return KUWAIT;
-		case 107:
-			return KYRGYZSTAN;
-		case 108:
-			return LAOS;
-		case 109:
-			return LATVIA;
-		case 110:
-			return LEBANON;
-		case 111:
-			return LESOTHO;
-		case 112:
-			return LIBERIA;
-		case 113:
-			return LIBYA;
-		case 114:
-			return LIECHTENSTIEN;
-		case 115:
-			return LITHUANIA;
-		case 116:
-			return LUXEMBOURG;
-		case 117:
-			return CHINA;
-		case 118:
-			return MACEDONIA;
-		case 119:
-			return MADAGASCAR;
-		case 120:
-			return MALAWI;
-		case 121:
-			return MALAYSIA;
-		case 122:
-			return MALDIVES;
-		case 123:
-			return MALI;
-		case 124:
-			return MALTA;
-		case 125:
-			return UNITEDKINGDOM;
-		case 126:
-			return UNITEDSTATES;
-		case 127:
-			return FRANCE;
-		case 128:
-			return MAURITANIA;
-		case 129:
-			return MAURITIUS;
-		case 130:
-			return MEXICO;
-		case 132:
-			return MOLDOVA;
-		case 134:
-			return MONGOLIA;
-		case 136:
-			return MOROCCO;
-		case 137:
-			return MOZAMBIQUE;
-		case 138:
-			return MYANMAR;
-		case 139:
-			return NAMIBIA;
-		case 141:
-			return NEPAL;
-		case 142:
-			return NETHERLANDS;
-		case 143:
-			return FRANCE;
-		case 144:
-			return NEWZEALAND;
-		case 145:
-			return NICARAGUA;
-		case 146:
-			return NIGER;
-		case 147:
-			return NIGERIA;
-		case 149:
-			return NORTHKOREA;
-		case 151:
-			return NORWAY;
-		case 152:
-			return OMAN;
-		case 153:
-			return PAKISTAN;
-		case 155:
-			return WESTBANKGAZASTRIP;
-		case 156:
-			return PANAMA;
-		case 157:
-			return PAPUANEWGUINEA;
-		case 158:
-			return PARAGUAY;
-		case 159:
-			return PERU;
-		case 160:
-			return PHILIPPINES;
-		case 161:
-			return POLAND;
-		case 163:
-			return UNITEDSTATES;
-		case 164:
-			return QATAR;
-		case 166:
-			return ROMANIA;
-		case 167:
-			return RUSSIA;
-		case 168:
-			return RWANDA;
-		case 169:
-			return NETHERLANDS;
-		case 173:
-			return SAUDIARABIA;
-		case 174:
-			return SENEGAL;
-		case 175:
-			return SERBIA;
-		case 176:
-			return SEYCHELLES;
-		case 177:
-			return SIERRALEONE;
-		case 178:
-			return SINGAPORE;
-		case 179:
-			return SLOVAKIA;
-		case 180:
-			return SLOVENIA;
-		case 181:
-			return UNITEDSTATES;
-		case 182:
-			return SOMALIA;
-		case 183:
-			return SOUTHAFRICA;
-		case 184:
-			return SOUTHKOREA;
-		case 185:
-			return SPAIN;
-		case 186:
-			return SRILANKA;
-		case 189:
-			return STKITTSNEVIS;
-		case 190:
-			return STLUCIA;
-		case 192:
-			return ITALY;
-		case 195:
-			return SUDAN;
-		case 196:
-			return SURINAME;
-		case 197:
-			return SWAZILAND;
-		case 198:
-			return SWEDEN;
-		case 199:
-			return SWITZERLAND;
-		case 200:
-			return SYRIA;
-		case 201:
-			return TAIWAN;
-		case 202:
-			return TAJIKISTAN;
-		case 203:
-			return TANZANIA;
-		case 204:
-			return TOGO;
-		case 205:
-			return THAILAND;
-		case 206:
-			return FIJI;
-		case 207:
-			return TRINIDADTOBAGO;
-		case 208:
-			return TUNISIA;
-		case 209:
-			return TURKEY;
-		case 210:
-			return TURKMENISTAN;
-		case 212:
-			return UNITEDKINGDOM;
-		case 213:
-			return UGANDA;
-		case 214:
-			return UKRAINE;
-		case 215:
-			return UNITEDARABEMIRATES;
-		case 216:
-			return UNITEDKINGDOM;
-		case 217:
-			return UNITEDSTATES;
-		case 218:
-			return URUGUAY;
-		case 219:
-			return UZBEKISTAN;
-		case 220:
-			return VANUATU;
-		case 221:
-			return VATICANCITY;
-		case 222:
-			return VENEZUELA;
-		case 223:
-			return VIETNAM;
-		case 225:
-			return UNITEDSTATES;
-		case 226:
-			return FRANCE;
-		case 228:
-			return YEMEN;
-		case 229:
-			return DRC;
-		case 230:
-			return ZAMBIA;
-		case 231:
-			return ZIMBABWE;
-		case 233:
-			return UNITEDKINGDOM;
-		case 235:
-			return YUGOSLAVIA;
-		case 236:
-			return CZECHOSLOVAKIA;
-		case 238:
-			return FRANCE;
-		case 347:
-			return EASTTIMOR;
-		case 349:
-			return MOROCCO;
-		case 351:
-		case 359:
-			return RUSSIA;
-		case 362:
-			return GERMANY;
-		case 377:
-			return YEMEN;
-		case 403:
-			return ZIMBABWE;
-		case 406:
-			return YEMEN;
-		case 428:
-			return VIETNAM;
-		case 499:
-			return GERMANY;
-		case 520:
-			return INDIA;
-		case 532:
-			return VANUATU;
-		case 603:
-			return UNITEDKINGDOM;
-		case 604:
-			return DRC;
-		case 605:
-			return REPUBLICOFCONGO;
-		case 1001:
-			return SERBIA;
-		case 1002:
-			return MONTENEGRO;
-		case 1003:
-			return KOSOVO;
-		case 1004:
-			return SOUTHSUDAN;
-		default:
-			return -1;
+//takes country code input as an int and returns the enumerated value of the country, ie the position in the vectors
+//in each node
+int Graph::countrycodes(int i){
+	switch (i){
+		case 4: return 0;
+		case 8: return 1;
+		case 12: return 2;
+		case 16: return 3;
+		case 20: return 4;
+		case 24: return 5;
+		case 28: return 6;
+		case 31: return 7;
+		case 32: return 8;
+		case 36: return 9;
+		case 40: return 10;
+		case 44: return 11;
+		case 48: return 12;
+		case 50: return 13;
+		case 51: return 14;
+		case 52: return 15;
+		case 56: return 16;
+		case 58: return 17;
+		case 60: return 18;
+		case 64: return 19;
+		case 68: return 20;
+		case 70: return 21;
+		case 72: return 22;
+		case 76: return 23;
+		case 84: return 24;
+		case 86: return 25;
+		case 90: return 26;
+		case 92: return 27;
+		case 96: return 28;
+		case 100: return 29;
+		case 104: return 30;
+		case 108: return 31;
+		case 112: return 32;
+		case 116: return 33;
+		case 120: return 34;
+		case 124: return 35;
+		case 132: return 36;
+		case 136: return 37;
+		case 140: return 38;
+		case 144: return 39;
+		case 148: return 40;
+		case 152: return 41;
+		case 156: return 42;
+		case 162: return 43;
+		case 166: return 44;
+		case 170: return 45;
+		case 174: return 46;
+		case 175: return 47;
+		case 178: return 48;
+		case 180: return 49;
+		case 184: return 50;
+		case 188: return 51;
+		case 191: return 52;
+		case 192: return 53;
+		case 196: return 54;
+		case 200: return 55;
+		case 203: return 56;
+		case 204: return 57;
+		case 208: return 58;
+		case 212: return 59;
+		case 214: return 60;
+		case 218: return 61;
+		case 222: return 62;
+		case 226: return 63;
+		case 231: return 64;
+		case 232: return 65;
+		case 233: return 66;
+		case 238: return 67;
+		case 242: return 68;
+		case 246: return 69;
+		case 251: return 70;
+		case 258: return 71;
+		case 260: return 72;
+		case 262: return 73;
+		case 266: return 74;
+		case 268: return 75;
+		case 270: return 76;
+		case 275: return 77;
+		case 276: return 78;
+		case 278: return 79;
+		case 280: return 80;
+		case 288: return 81;
+		case 292: return 82;
+		case 296: return 83;
+		case 300: return 84;
+		case 304: return 85;
+		case 308: return 86;
+		case 316: return 87;
+		case 320: return 88;
+		case 324: return 89;
+		case 328: return 90;
+		case 332: return 91;
+		case 340: return 92;
+		case 344: return 93;
+		case 348: return 94;
+		case 352: return 95;
+		case 360: return 96;
+		case 364: return 97;
+		case 368: return 98;
+		case 372: return 99;
+		case 376: return 100;
+		case 381: return 101;
+		case 384: return 102;
+		case 388: return 103;
+		case 392: return 104;
+		case 398: return 105;
+		case 400: return 106;
+		case 404: return 107;
+		case 408: return 108;
+		case 410: return 109;
+		case 414: return 110;
+		case 417: return 111;
+		case 418: return 112;
+		case 422: return 113;
+		case 426: return 114;
+		case 428: return 115;
+		case 430: return 116;
+		case 434: return 117;
+		case 440: return 118;
+		case 442: return 119;
+		case 446: return 120;
+		case 450: return 121;
+		case 454: return 122;
+		case 458: return 123;
+		case 462: return 124;
+		case 466: return 125;
+		case 470: return 126;
+		case 478: return 127;
+		case 480: return 128;
+		case 484: return 129;
+		case 490: return 130;
+		case 496: return 131;
+		case 498: return 132;
+		case 499: return 133;
+		case 500: return 134;
+		case 504: return 135;
+		case 508: return 136;
+		case 512: return 137;
+		case 516: return 138;
+		case 520: return 139;
+		case 524: return 140;
+		case 528: return 141;
+		case 530: return 142;
+		case 531: return 143;
+		case 533: return 144;
+		case 534: return 145;
+		case 535: return 146;
+		case 540: return 147;
+		case 548: return 148;
+		case 554: return 149;
+		case 558: return 150;
+		case 562: return 151;
+		case 566: return 152;
+		case 570: return 153;
+		case 574: return 154;
+		case 579: return 155;
+		case 580: return 156;
+		case 583: return 157;
+		case 584: return 158;
+		case 585: return 159;
+		case 586: return 160;
+		case 591: return 161;
+		case 598: return 162;
+		case 600: return 163;
+		case 604: return 164;
+		case 608: return 165;
+		case 612: return 166;
+		case 616: return 167;
+		case 620: return 168;
+		case 624: return 169;
+		case 626: return 170;
+		case 634: return 171;
+		case 642: return 172;
+		case 643: return 173;
+		case 646: return 174;
+		case 652: return 175;
+		case 654: return 176;
+		case 659: return 177;
+		case 660: return 178;
+		case 662: return 179;
+		case 666: return 180;
+		case 670: return 181;
+		case 674: return 182;
+		case 678: return 183;
+		case 682: return 184;
+		case 686: return 185;
+		case 688: return 186;
+		case 690: return 187;
+		case 694: return 188;
+		case 697: return 189;
+		case 699: return 190;
+		case 702: return 191;
+		case 703: return 192;
+		case 704: return 193;
+		case 705: return 194;
+		case 706: return 195;
+		case 710: return 196;
+		case 711: return 197;
+		case 716: return 198;
+		case 724: return 199;
+		case 728: return 200;
+		case 729: return 201;
+		case 736: return 202;
+		case 740: return 203;
+		case 748: return 204;
+		case 752: return 205;
+		case 757: return 206;
+		case 760: return 207;
+		case 762: return 208;
+		case 764: return 209;
+		case 768: return 210;
+		case 772: return 211;
+		case 776: return 212;
+		case 780: return 213;
+		case 784: return 214;
+		case 788: return 215;
+		case 792: return 216;
+		case 795: return 217;
+		case 796: return 218;
+		case 798: return 219;
+		case 800: return 220;
+		case 804: return 221;
+		case 807: return 222;
+		case 810: return 223;
+		case 818: return 224;
+		case 826: return 225;
+		case 834: return 226;
+		case 842: return 227;
+		case 849: return 228;
+		case 854: return 229;
+		case 858: return 230;
+		case 860: return 231;
+		case 862: return 232;
+		case 876: return 233;
+		case 882: return 234;
+		case 887: return 235;
+		case 891: return 236;
+		case 894: return 237;
+
 	}
-}
-
-
-// note to implementor, to find what number to return, simply type the name of
-// the country you found in all caps accoring to the spelling in countries.h
-// this is an enum which aliases country names to their position in the ajacency
-// matrix
-//
-// you will probably also have to write a very long if statement since switches
-// refuse to take strings, I tried to do a hash as an experiment for this, but
-// the compiled apparently doesn't like this as the hashing wasn't a constant at
-// compile time (even though it totally was), maybe you could get around this
-// with a preprocessor macro instead, there should be about 190 condition, make
-// sure to use || and not make a case for every group...
-
-//XXX implement me
-int Graph::terrorcodes(std::string s) {
-	std::string tmpstring = s;
-	return -1;
 }
