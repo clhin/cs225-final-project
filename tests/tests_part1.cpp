33b0df636a45a1a5ba2ab606859483b3f017413e
johnhof3 <95899793+johnhof3@users.noreply.github.com>
2022-12-11 14:02:52 -0800

    no longer building graph for every testcase

--- tests/tests_part1.cpp
+++ tests/tests_part1.cpp
@@ -2,32 +2,29 @@
 #include <cmath>
 #include "graph.h"
 
+Graph nations = Graph("../BACI_HS17_Y2020_V202201.csv");
 
 //first trade pair only one value
 TEST_CASE("buildGraph1", "[weight=10][part1]"){
-    Graph nations = Graph("../BACI_HS17_Y2020_V202201.csv");
     REQUIRE(27.82f == nations.Exports(4,31));
 }
 //empty trade pair
 TEST_CASE("buildGraph2", "[weight=10][part1]"){
-    Graph nations = Graph("../BACI_HS17_Y2020_V202201.csv");
     REQUIRE(0.0f == nations.Exports(4,12));
 }
 //large trade pair
 TEST_CASE("buildGraph3", "[weight=10][part1]"){
-    Graph nations = Graph("../BACI_HS17_Y2020_V202201.csv");
     REQUIRE(44844.316f == nations.Exports(76,226));
 }
 //country with itself should be zero
 TEST_CASE("buildGraph4", "[weight=10][part1]"){
-    Graph nations = Graph("../BACI_HS17_Y2020_V202201.csv");
     REQUIRE(0.0f == nations.Exports(100,100));
 }
 //another large trade pair, using ceil because float is only accurate to 7 digits
 TEST_CASE("buildGraph5", "[weight=10][part1]"){
-    Graph nations = Graph("../BACI_HS17_Y2020_V202201.csv");
     REQUIRE(ceil(148533.361f) == ceil(nations.Exports(112,31)));
 }
+
 /*
 //Djikstra Test Cases XXX FIXME XXX
 TEST_CASE("djikstra1", "[weight=10][part1]") { //Tests Undirected Simple Graph
