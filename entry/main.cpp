79eac0e24cddba8cc3e4af0158c227dfa8ff200a
Rithvik Kopparapu <rithvik9@illinois.edu>
2022-12-11 22:51:26 +0000

    Modifying main to have the right file path

--- entry/main.cpp
+++ entry/main.cpp
@@ -2,7 +2,7 @@
 #include "graph.h"
 int main() {
 	//std::cout << "hello world" << std::endl;
-	Graph nations = Graph("BACI_HS17_Y2017_V202201.csv");
+	Graph nations = Graph("BACI_HS17_Y2020_V202201.csv");
     std::cout << nations.Exports(0,7);
 	nations.pagerank(100);
 }
