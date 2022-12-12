7a98f711863622edb89591e752f52d47c8cf9f36
johnhof3 <95899793+johnhof3@users.noreply.github.com>
2022-12-11 20:20:11 -0800

    adds visualizations.h

---
+++ src/visualizations.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <matplot/matplot.h>
+
+/*
+
+void balanceOfTradeMap(int country, Graph g);
+void DijkstraVisualization(int country, std::vector<float> countries);
+void PageRankVisualization(int country, std::vector<int> rankings);
+
+
+*/
