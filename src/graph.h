5c6aef447ce44978d1b51cd4dbad04ae52db256a
johnhof3 <95899793+johnhof3@users.noreply.github.com>
2022-12-11 20:22:02 -0800

    Update graph.h

--- src/graph.h
+++ src/graph.h
@@ -6,20 +6,24 @@
 #include <functional>
 #include <queue>
 #include <float.h>
-#include <algorithm>
 #include <bits/stdc++.h>
 
 class Graph {
 	public:
-	Graph();
+		Graph();
 	    Graph(const std::string& db_fpath);
         float Exports(int exporter, int importer);
 		std::vector<int> BFS(int start);
 		std::vector<float> pagerank(int i);
-		std::vector<int> pageranksearch(int country, std::vector<float> rank);
-		std::vector<float> Djikstra(int idx);
+		std::vector<float> Djikstra(int country_code);
 		std::vector<float> TestDjikstra(int country_code, std::vector<std::vector<float>> test_graph);
+		std::vector<int> pageranksearch(int country, std::vector<float> rank);
 	private:
 		int countrycodes(int i);
 	    std::vector<std::vector<float>> graph;
 };
+
+std::string getCountryName(int index);
+std::vector<float> getCountryCoords(int index);
+std::vector<std::string> idxVectToString(std::vector<int> countryIdxs);
+int stringToIdx(std::string country);
