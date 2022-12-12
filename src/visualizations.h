0242ca478e3d4c68f8422c29f45f266a64ac9299
johnhof3 <95899793+johnhof3@users.noreply.github.com>
2022-12-12 13:56:36 -0600

    Update visualizations.h

--- src/visualizations.h
+++ src/visualizations.h
@@ -5,7 +5,6 @@
 #include <matplot/matplot.h>
 
 void balanceOfTradeMap(int country, Graph g);
-void DijkstraVisualization(int country, std::vector<float> countries);
 void PageRankVisualization(int country, std::vector<int> rankings);
 
 
